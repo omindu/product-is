b42097b5c0bb2c96d073bbc97c27eb5dc53863b6
omindu <omindu@wso2.com>
2017-11-03 00:39:32 -0700

    Adding implicit support and JWT display for playground app.

--- modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/OAuth2ClientServlet.java
+++ modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/OAuth2ClientServlet.java
@@ -19,7 +19,6 @@
 package org.wso2.sample.identity.oauth2;
 
 import java.io.IOException;
-
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
